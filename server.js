d6a653c272fbadca412d95d258200012455b7c44
tuath-pi <pi@example.com>
2022-08-07 22:18:00 +0100

    TD content is rewritten if a custom ip address:port is used

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-console.log(process.argv);
 const mDnsSd = require('node-dns-sd');
 const LinkSmartThingDirectory = require('link_smart_thing_directory');
 const axios = require('axios');
@@ -6,8 +5,9 @@ const axios = require('axios');
 let thingsLocation = new URL("http://localhost:8080/");
 // override with command line arg host=URL
 let customLocation = null;
+// test vale customLocation = new URL("http://192.168.86.30:8888/");
 if ( process.argv.length > 2) {
-    console.log(process.argv[2].split('=')[1]);
+    console.log("Using custom base URL: ", process.argv[2].split('=')[1]);
     customLocation = new URL( process.argv[2].split('=')[1] )
 }
 thingsLocation = customLocation ? customLocation : thingsLocation;
@@ -28,14 +28,20 @@ axios.get(thingsLocation.toString())  //base url returns a list of TD urls
         .then(axios.spread((...responses)=> {
             const tds = [];                         // process each response
             for ( var response of responses) {
-                tds.push( response.data);
+                if (!customLocation) tds.push( response.data)
+                else {
+                    let customHost = customLocation.host;
+                    let originalHost = new URL(response.config.url).host;
+                    datastring = JSON.stringify(response.data)
+                    tds.push ( JSON.parse(datastring.replace(new RegExp(originalHost, 'g'), customHost) ))
+                }
                 }
             if ( tds.length == 0 ){
                 console.log("No valid TD at URL: " + thingsLocation);
             }
             else {
                 console.log("Things Descriptors to register:");
-                console.log(tds);
+                console.log(JSON.stringify(tds));
                 registerWithADirectory(tds);            // register the TD data with a directory
             }
         }))
@@ -64,7 +70,7 @@ function registerWithADirectory(tds) {
             if (error) {
                 console.error(error);
             } else {
-                console.log('Directory: ' + JSON.stringify(directory) + '. TD registration successfull.');
+                console.log('Directory: ' + JSON.stringify(directory) + '. TD registration successful.');
             }
             });
         }
